f53a049c177cd1c9c916b349a5036e7504985605
awnish462 <82871890+awnish462@users.noreply.github.com>
2022-05-24 20:50:06 +0530

    Day 3 Completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  final int days = 30;
+  final String name = "Awnish";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          child: Text("Welcome to $days days of coding"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
