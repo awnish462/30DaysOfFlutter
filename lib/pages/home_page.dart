f026d30fbe897ae703fd1e154430ce93508196b2
awnish462 <82871890+awnish462@users.noreply.github.com>
2022-06-23 22:00:37 +0530

    Day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+// import 'dart:html';
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:convert';
+
 import 'package:flutter_catelog/model/catelog.dart';
-import 'package:flutter_catelog/pages/widgets/ItemWidget.dart';
 import 'package:flutter_catelog/pages/widgets/drawer.dart';
+import 'package:flutter_catelog/pages/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -37,45 +43,114 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catelog App"),
-      ),
-      //listview builder gives recyclerview to render items
-      body: (CatelogItem.item != null && CatelogItem.item!.isNotEmpty)
-          ? GridView.builder(
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    // crossAxisSpacing: 16,
-                    // mainAxisExtent: 16
-                  ),
-                  
-              itemBuilder: (context, index) {
-                final item = CatelogItem.item![index];
-                return Card(
-                    // clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    child: GridTile(
-                        header: Container(
-                          child: Text(item.name,style: TextStyle(color: Colors.white),),
-
-                        decoration: BoxDecoration(
-                          color: Colors.purple
-                        ),
-                        padding: EdgeInsets.all(2),),
-                        footer: Text(
-                          item.price.toString(),
-                        ),
-                        child: Image.network(item.image)));
-              },
-              itemCount: CatelogItem.item!.length,
-              )
-
-          : Center(
-              child: CircularProgressIndicator(),
+        backgroundColor: MyTheme.creamColor,
+        body: SafeArea(
+          child: Container(
+            padding: Vx.m24,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatelogHeader(),
+                if (CatelogItem.item != null && CatelogItem.item!.isNotEmpty)
+                  CatelogList().expand()
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+              ],
             ),
-      drawer: MyDrawer(),
+          ),
+        ));
+  }
+}
+
+class CatelogHeader extends StatelessWidget {
+  const CatelogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catelog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending products".text.xl2.make(),
+      ],
     );
+  }
+}
+
+class CatelogList extends StatelessWidget {
+  const CatelogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatelogItem.item?.length,
+      itemBuilder: (context, index) {
+        final catelog = CatelogItem.item![index];
+        //catelogitems is widget which we will make
+        return CatelogItems(
+          catelog: catelog,
+        );
+      },
+    );
+  }
+}
+
+class CatelogItems extends StatelessWidget {
+  final Item catelog;
+  const CatelogItems({
+    Key? key,
+    required this.catelog,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatelogImage(image: catelog.image),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catelog.name.text.xl.color(MyTheme.darkBluishColor).bold.make(),
+            catelog.desc.text.textStyle(context.captionStyle).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: EdgeInsets.zero,
+              children: [
+                "\$${catelog.price}".text.bold.xl.make(),
+                ElevatedButton(onPressed: (){}, child:"Buy".text.make(),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(MyTheme.darkBluishColor),
+                shape: MaterialStateProperty.all(StadiumBorder())
+                ),
+                ),
+              ],
+            ).pOnly(right: 8.0)
+          ],
+        ))
+      ],
+    )).white.roundedLg.square(130).make().py16();
+  }
+}
+
+class CatelogImage extends StatelessWidget {
+  const CatelogImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .p8
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context);
   }
 }
--- lib/pages/widgets/themes.dart
+++ lib/pages/widgets/themes.dart
@@ -9,8 +9,9 @@ class MyTheme {
           iconTheme: IconThemeData(color: Colors.black),
           titleTextStyle: TextStyle(color: Colors.black, fontSize: 20)));
 
-        static  ThemeData darkTheme() => ThemeData(
-            brightness: Brightness.light,
+  static ThemeData darkTheme() => ThemeData(
+        brightness: Brightness.light,
       );
-
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
